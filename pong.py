ffdf3f0951ab851bb9c754a3d192fee0527197e2
Lane <joshlane3@gmail.com>
2023-02-28 08:27:08 -0500

    finish up game

--- pong.py
+++ pong.py
@@ -4,6 +4,8 @@ import turtle
 ball = turtle.Turtle()
 ball.shape("circle")
 ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 #game screen
 turtle.setup(400, 300)
@@ -20,3 +22,30 @@ paddle1.goto(-350, 0)
 paddle1.dy = 0
 
 #right paddle
+paddle2 = turtle.Turtle()
+paddle2.shape("square")
+paddle2.color("blue")
+paddle2.shapesize(stretch_wid=5, stretch_len=1)
+paddle2.penup()
+paddle2.goto(350, 0)
+paddle2.dy = 0
+
+#game rules
+game_over = False
+winner = None
+points = {
+    "player1": 0,
+    "player2": 0
+}
+game_rules = {
+    "max_points": 5,
+    "ball_speed": 3
+}
+
+#score display
+score_display = turtle.Turtle()
+score_display.color("white")
+score_display.penup()
+score_display.hideturtle()
+score_display.goto(0, 260)
+score_display.write("Player 1: 0 Player 2: 0", align="center", font=("Arial", 24, "normal"))
