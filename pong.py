6c5f02b362681d8fc2904075ee6d69da4b938e84
Lane <joshlane3@gmail.com>
2023-02-28 08:20:14 -0500

    left paddle

--- pong.py
+++ pong.py
@@ -9,3 +9,14 @@ ball.color("white")
 turtle.setup(400, 300)
 #background color
 turtle.bgcolor("black")
+
+#left paddle
+paddle1 = turtle.Turtle()
+paddle1.shape("square")
+paddle1.color("red")
+paddle1.shapesize(stretch_wid=5, stretch_len=1) 
+paddle1.penup()
+paddle1.goto(-350, 0)
+paddle1.dy = 0
+
+#right paddle
